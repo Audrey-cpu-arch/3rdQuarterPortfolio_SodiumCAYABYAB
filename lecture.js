8e71d4c22dbe7c84b36065262119b7a921dcffb5
Audrey-cpu-arch <b2029afscayabyab@pshs.edu.ph>
2026-01-12 14:18:50 +0800

    Yuta x Maki

--- lecture.js
+++ lecture.js
@@ -2,6 +2,7 @@
 //    localStorage.getItem(key) - retrieves data
 //    localStorage.removeItem(key) - deletes one item
 //    localStorage.clear() - clears all items
-//    localStorage.clear() - clears all items
 
 // Bulma is a CSS template
+
+//check gmail for all info from the lecture
