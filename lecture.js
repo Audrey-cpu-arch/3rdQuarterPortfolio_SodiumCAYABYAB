0de3b6986b3d3c7d3d676dcaaf7acb93f56a9cc9
Audrey-cpu-arch <b2029afscayabyab@pshs.edu.ph>
2026-01-12 13:24:24 +0800

    Asa x Denji

---
+++ lecture.js
@@ -0,0 +1,5 @@
+//    localStorage.setItem(key, value) - saves data
+//    localStorage.getItem(key) - retrieves data
+//    localStorage.removeItem(key) - deletes one item
+//    localStorage.clear() - clears all items
+//    localStorage.clear() - clears all items
