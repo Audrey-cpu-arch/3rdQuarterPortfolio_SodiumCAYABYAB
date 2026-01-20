5bfd819f1e64289739521dc7444792166f614267
Audrey-cpu-arch <b2029afscayabyab@pshs.edu.ph>
2026-01-20 20:07:28 +0800

    irrelevant

--- lecture.js
+++
@@ -1,8 +0,0 @@
-//    localStorage.setItem(key, value) - saves data
-//    localStorage.getItem(key) - retrieves data
-//    localStorage.removeItem(key) - deletes one item
-//    localStorage.clear() - clears all items
-
-// Bulma is a CSS template
-
-//check gmail for all info from the lecture
